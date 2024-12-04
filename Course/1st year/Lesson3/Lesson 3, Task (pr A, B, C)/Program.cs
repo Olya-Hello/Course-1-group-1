03008487e907d36145577a1001844e29d026ba7b
Olya-Hello <olgayurchikova10@gmail.com>
2024-12-04 12:09:40 +1000

    Практика третьего урока

---
+++ Course/1st year/Lesson3/Lesson 3, Task (pr A, B, C)/Program.cs
@@ -0,0 +1,33 @@
+﻿// практика A
+
+int[] numbers = new int[5];
+
+
+string[] numbers0 = new string[3];
+
+
+// практика B
+
+int[] numbers1 = {69, 52, 42, 89, 14};
+
+int element = numbers1[1]; 
+
+
+numbers1[2] = 100;
+
+
+Console.WriteLine(numbers1.Length);
+
+
+// практика С
+
+string[] word = new string[4];
+
+
+int[] numbers2 = new int[5];
+
+
+string[] word0 = new string[3];
+
+string[] word1 = {"огуречек", "помидорчек", "перчик", "салатек", "тыковка"};
+
