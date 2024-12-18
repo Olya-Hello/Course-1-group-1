489cccbe9e3a6e066fa8771971f5a6b270e78b9e
Olya-Hello <olgayurchikova10@gmail.com>
2024-12-18 17:44:57 +1000

    задание урока 4

---
+++ Course/1st year/Lesson4/Lesson 4, task/2/Program.cs
@@ -0,0 +1,24 @@
+﻿//№2
+
+int[] name = {89, 2, 23, 4, 88};
+
+Array.Sort(name);
+
+Console.WriteLine(name[^1]);
+
+//или
+
+
+int[] name1 = {1, 230, 17, 90 };
+
+int max = name1[0];
+
+for(int i = 0; i < name1.Length; i++)
+{
+    if(name1[i] > max)
+    {
+        max = name1[i];
+    }
+}
+
+Console.WriteLine(max);
---
+++ Course/1st year/Lesson4/Lesson 4, task/4/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] mass = {1, 22, 3, 4, 5, 6, 78, 91};
+
+foreach(int i in mass)
+{
+    if(i %2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
