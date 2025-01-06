d9271714eaacc0015dec0d10387fcf4b82b4b7eb
Olya-Hello <olgayurchikova10@gmail.com>
2025-01-06 22:27:13 +1000

    задание урока 4

---
+++ Course/1st year/Lesson4/Lesson 4, task/6/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] mass = {"шел Штирлиц и упал ","но Упала сбила машина ", "дальше Штрирлиц пошёл один"};
+
+Console.WriteLine("Какой элемент массива вывести? всего три (1, 2, 3)");
+
+int you = int.Parse(Console.ReadLine());
+
+if(you == 1)
+{
+    Console.WriteLine(mass[0]);
+}
+else if(you == 2)
+{
+    Console.WriteLine(mass[1]);
+}
+else if(you == 3)
+{
+    Console.WriteLine(mass[2]);
+}
+else if(you == 4)
+{
+    Console.WriteLine("введён некорректный номер");
+    Console.WriteLine("хаха. на продолжение надеялся? :>");
+}
+else
+{
+    Console.WriteLine("введён некорректный номер");
+}
